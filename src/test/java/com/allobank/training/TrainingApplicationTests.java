3e4a48cebd4feb46abbd8e383d728fc83a078dfd
O2601005 <kevin.chandra_ext@allobank.com>
2026-04-24 10:43:58 +0700

    initialization

---
+++ src/test/java/com/allobank/training/TrainingApplicationTests.java
@@ -0,0 +1,11 @@
+package com.allobank.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
